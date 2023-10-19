c27383212185031ff9b5f432b3908958fed5cb4c
Henry <henryukomah@gmail.com>
2023-10-19 01:02:31 +0100

    setting up heroku

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['natures-narrative.herokuapp.com']
+
+django_heroku.settings(locals())
 
 
 # Application definition
