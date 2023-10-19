6fa57fc0200267ab737bb932c36c36fddf1f77c5
Henry <henryukomah@gmail.com>
2023-10-19 01:28:37 +0100

    setting up heroku

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -94,17 +94,17 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'blog',
-    'USER': 'postgres',
-    'PASSWORD': os.getenv('DB_PASSWORD')
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'blog',
+#     'USER': 'postgres',
+#     'PASSWORD': os.getenv('DB_PASSWORD')
+#     }
 # }
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 
 
